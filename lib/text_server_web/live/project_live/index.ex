e17f43a6829ddea653daa497025608aa80e056f8
Charles Pletcher <c@plet.ch>
2022-07-24 18:59:19 -0400

    Style ProjectIndex view
    Also improve routing for ProjectUserIndex and editing projects (work
        continues there)

--- lib/text_server_web/live/project_live/index.ex
+++ lib/text_server_web/live/project_live/index.ex
@@ -1,8 +1,8 @@
 defmodule TextServerWeb.ProjectLive.Index do
   use TextServerWeb, :live_view
 
-  alias TextServer.Accounts
   alias TextServer.Projects
+  alias TextServerWeb.ProjectLive.ProjectComponents
 
   @impl true
   def mount(_params, _session, socket) do
@@ -14,25 +14,10 @@ defmodule TextServerWeb.ProjectLive.Index do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Project")
-    |> assign(:project, Projects.get_project!(id))
-  end
-
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Projects")
     |> assign(:project, nil)
-  end
-
-  defp apply_action(socket, :user_project_index, %{"user_id" => user_id}) do
-    user = Accounts.get_user!(user_id) |> TextServer.Repo.preload(:user_projects)
-
-    socket
-    |> assign(:page_title, "My Projects")
-    |> assign(:project_collection, user.user_projects)
-    |> assign(:user_id, user_id)
   end
 
   @impl true

064afef1ea63670a819fcc962ce681f666696da0
Charles Pletcher <c@plet.ch>
2022-11-22 23:19:54 -0500

    Debug and fix buggy loading state on homepage/non-LiveViews
    Update dependencies, continue working on tests

--- config/config.exs
+++ config/config.exs
@@ -55,7 +55,7 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 config :tailwind,
-  version: "3.1.6",
+  version: "3.2.4",
   default: [
     args: ~w(
       --config=tailwind.config.js

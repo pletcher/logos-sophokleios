eb58f096e657a6bcf1cb0cba54aa62785c2defa2
Charles Pletcher <c@plet.ch>
2022-12-05 13:41:47 -0500

    Start paying attention to versioning

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TextServer.MixProject do
   def project do
     [
       app: :text_server,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers() ++ [:rambo],

cbccd005d5cca8bb3ce0679cb0b3f75515463f1f
Charles Pletcher <c@plet.ch>
2022-12-12 15:02:07 -0500

    Bump phoenix version

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule TextServer.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.6.6"},
+      {:phoenix, "~> 1.6.15"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, "~> 0.16.3"},

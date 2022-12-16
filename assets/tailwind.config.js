5cbbdc247487e7f5626b4d15995d537c3b8b2386
Charles Pletcher <c@plet.ch>
2022-12-16 13:20:36 -0500

    Use wikimedia image for Pausanias cover

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         "alexandria-scrolls":
           "linear-gradient(180deg,rgba(0,0,0,.8) 0,rgba(0,0,0,.6) 30%,rgba(0,0,0,.3)), url('/images/alexandria_scrolls_commentaries.jpg')",
         pausanias:
-          "url('/images/zeus_olympios_agrigento.jpg')",
+          "url('/images/Pausanias_Description_of_Greece.jpg')",
       },
     },
   },

e67cae0e467fca5d16cc84927f66abcd1b30044d
Charles Pletcher <c@plet.ch>
2023-01-06 23:21:23 -0500

    Update token expiration

--- lib/text_server_web/plugs/api.ex
+++ lib/text_server_web/plugs/api.ex
@@ -4,7 +4,7 @@ defmodule TextServerWeb.Plugs.API do
   import Plug.Conn
   import Phoenix.Controller
 
-  @one_month 30 * 24 * 60 * 60
+  @one_year 365 * 24 * 60 * 60
 
   def init(opts), do: opts
 
@@ -32,13 +32,21 @@ defmodule TextServerWeb.Plugs.API do
     end
   end
 
+  def generate_token(user_id) do
+    Phoenix.Token.sign(
+      TextServerWeb.Endpoint,
+      TextServerWeb.Endpoint.config(:secret_key_base),
+      user_id
+    )
+  end
+
   @spec verify_token(nil | binary) :: {:error, :expired | :invalid | :missing} | {:ok, any}
   def verify_token(token) do
     Phoenix.Token.verify(
       TextServerWeb.Endpoint,
       TextServerWeb.Endpoint.config(:secret_key_base),
       token,
-      max_age: @one_month
+      max_age: @one_year
     )
   end
 
